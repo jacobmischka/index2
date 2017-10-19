1b8db5e6e6e65cf0a9259aeaeb46845165c6d84f
Jacob Mischka <jacob@mischka.me>
2017-10-18 20:03:40 -0500

    Add css config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
@@ -29,10 +30,34 @@ module.exports = {
 						}
 					}
 				]
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								ident: 'postcss',
+								plugins: [
+									require('postcss-cssnext')
+								]
+							}
+						}
+					]
+				})
 			}
 		]
 	},
 	plugins: [
+		new ExtractTextPlugin('styles.css'),
 		new HtmlWebpackPlugin({
 			filename: 'index.php',
 			title: 'index2',
