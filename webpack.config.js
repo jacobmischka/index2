448635c5df111f03f381514067171ccab344db2d
Jacob Mischka <jacob@mischka.me>
2017-11-04 00:08:34 -0500

    Use uglify-es to minify production build
    Looks like pretty-bytes doesn't play well with uglifyjs2.

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,91 @@
 /* eslint-env node */
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	target: 'web',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.html$/,
-				exclude: /index\.html$/,
-				use: [
-					'babel-loader',
-					'svelte-loader'
-				]
-			},
-			{
-				test: /\.svg$/,
-				exclude: /node_modules/,
-				use: 'svg-inline-loader'
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2)$/,
-				include: /node_modules/,
-				use: 'base64-inline-loader'
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								importLoaders: 1
-							}
-						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								ident: 'postcss',
-								plugins: [
-									require('postcss-cssnext')
-								]
-							}
-						}
-					]
-				})
-			}
+module.exports = env => {
+	const productionPlugins = env.production
+		? [
+			new webpack.DefinePlugin({
+				NODE_ENV: 'production'
+			}),
+			new UglifyJSPlugin()
 		]
-	},
-	plugins: [
-		new ExtractTextPlugin('styles.css'),
-		new HtmlWebpackPlugin({
-			filename: 'index.php',
-			title: 'index2',
-			template: './src/index.php',
-			xhtml: true,
-			inlineSource: '.(js|css)$'
-		}),
-		new HtmlWebpackInlineSourcePlugin()
-	],
-	devtool: 'source-map'
+		: [];
+
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'bundle.js'
+		},
+		target: 'web',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				},
+				{
+					test: /\.html$/,
+					exclude: /index\.html$/,
+					use: [
+						'babel-loader',
+						'svelte-loader'
+					]
+				},
+				{
+					test: /\.svg$/,
+					exclude: /node_modules/,
+					use: 'svg-inline-loader'
+				},
+				{
+					test: /\.(eot|svg|ttf|woff|woff2)$/,
+					include: /node_modules/,
+					use: 'base64-inline-loader'
+				},
+				{
+					test: /\.css$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+									importLoaders: 1
+								}
+							},
+							{
+								loader: 'postcss-loader',
+								options: {
+									ident: 'postcss',
+									plugins: [
+										require('postcss-cssnext')
+									]
+								}
+							}
+						]
+					})
+				}
+			]
+		},
+		plugins: [
+			...productionPlugins,
+			new ExtractTextPlugin('styles.css'),
+			new HtmlWebpackPlugin({
+				filename: 'index.php',
+				title: 'index2',
+				template: './src/index.php',
+				xhtml: true,
+				inlineSource: '.(js|css)$'
+			}),
+			new HtmlWebpackInlineSourcePlugin()
+		],
+		devtool: 'source-map'
+	};
+
 };
